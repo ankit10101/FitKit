1c78052814709a2f801861876d8d4410f28a978f
Ankit Bajaj <ankitbajaj333@gmail.com>
2019-04-23 10:18:57 +0530

    Added logic for SignUp Activity
    Signed-off-by: Ankit Bajaj <ankitbajaj333@gmail.com>

--- app/src/main/java/com/example/fitkit/ui/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/fitkit/ui/activity/SignUpActivity.kt
@@ -1,13 +1,41 @@
 package com.example.fitkit.ui.activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.example.fitkit.MainActivity
 import com.example.fitkit.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        btnSignUp.setOnClickListener {
+            val email = etEmailSignUp.text.toString()
+            val password = etPasswordSignUp.text.toString()
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(baseContext, "Please Enter Email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(baseContext, "Please Enter PassWord", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Registration Completed", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
+                    it.printStackTrace()
+                }
+        }
     }
 }
